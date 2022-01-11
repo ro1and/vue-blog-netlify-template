997021bc8d614f1d65e4fc5ebb53a04cc685305d
Roland Wezenbeek <roland1385@gmail.com>
2022-01-12 00:09:21 +0100

    Update BlogFooter.vue

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        Copyright 2022 <a href="https://opensea.io/collection/inuft" target="_blank">InuFT</a>
+        © 2022 InuFT
       </div>
     </footer>
   </transition>
