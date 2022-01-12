6fbcfd12fdfc1287a7762851bb9cf670ce94a361
Roland Wezenbeek <roland1385@gmail.com>
2022-01-12 21:03:53 +0100

    Update BlogFooter.vue

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        <a href="https://www.instagram.com/inuftatopensea" target="_blank">Instagram</a> • <a href="https://twitter.com/inuftatopensea" target="_blank">Twitter</a> • <a href="https://opensea.io/collection/inuft" target="_blank">OpenSea</a> • © 2022 InuFT
+        <a href="https://www.instagram.com/inuftatopensea" target="_blank">Instagram</a> • <a href="https://twitter.com/inuftatopensea" target="_blank">Twitter</a> • <a href="https://opensea.io/collection/inuft" target="_blank">OpenSea</a>
       </div>
     </footer>
   </transition>
